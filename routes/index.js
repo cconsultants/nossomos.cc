05e5b99d57176acd970975f7205b7fe0f74d1a4c
Djalma Araujo <djalma.araujo@gmail.com>
2016-12-16 20:04:44 -0300

    Make it simple

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,3 @@
-var sendgrid = require('sendgrid')(process.env.SMTP_MAILER_USERNAME, process.env.SMTP_MAILER_PASSWORD);
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
   res.render('index', { title: 'Code Consultants | ' });
-};
-
-exports.postContact = function(req, res){
-  body = "MESSAGE FROM NOSSOMOS.CC: <br /><br />";
-  body += "Name: " + req.body.name + "<br />";
-  body += "Email: " + req.body.email + "<br />";
-  body += "Message: <br />";
-  body += req.body.message.replace("\n", "<br>")
-  body += "<br /><br />";
-
-  var payload   = {
-    to      : 'djalma@nossomos.cc',
-    from    : req.body.email,
-    subject : '[CC Website] Contact Form - ' + req.body.name,
-    html    : body
-  }
-
-  sendgrid.send(payload, function(err, json) {
-    console.log(err, json);
-    res.json({ success: (err) ? false : true });
-  });
 };
